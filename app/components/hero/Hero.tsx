ceae8f0ae44269bf0d829f044c1ce22581d4e5ce
RodyZack07 <wildanad466@gmail.com>
2026-01-07 08:06:28 +0700

    fix hero

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -6,15 +6,15 @@ import test from "../../public/test.jpg";
 const Hero = () => {
   return (
     <section className="w-full py-20">
-      <div className="min-w-full mx-5 px-0 grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-        <div className="mb-200">
+      <div className="max-w-[1400px] mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-20 items-center">
+
+        {/* TEXT KIRI */}
+        <div className="mb-">
           <h1 className="text-4xl lg:text-5xl font-bold text-black mb-6 leading-tight">
             Your Vault of <br /> Confidentiality
           </h1>
 
-          <p className="text-gray-700 mb-4">
-            The fortress for your secrets.
-          </p>
+          <p className="text-gray-700 mb-4">The fortress for your secrets.</p>
 
           <p className="text-gray-700 mb-4">
             Control who sees what. Track every move. Lock it down.
@@ -30,29 +30,23 @@ const Hero = () => {
           </button>
         </div>
 
-        {/* ===== IMAGE GRID KANAN ===== */}
-        <div className="grid grid-cols-2 grid-rows-2 gap-4 mb-220">
-          {/* kecil kiri atas */}
-          <Image
-            src={test}
-            alt="image 1"
-            className="w-full h-full object-cover rounded-xl"
-          />
+              {/* IMAGE GRID KANAN */}
+        <div className="grid grid-cols-2 grid-rows-2 gap-4
+                        min-h-[300px] lg:min-h-[360px]">
+          
+          <div className="relative rounded-xl overflow-hidden">
+            <Image src={test} alt="image 1" fill className="object-cover" />
+          </div>
 
-          {/* besar kanan */}
-          <Image
-            src={test}
-            alt="image big"
-            className="w-full h-full object-cover rounded-xl row-span-2"
-          />
+          <div className="relative rounded-xl overflow-hidden row-span-2">
+            <Image src={test} alt="image big" fill className="object-cover" />
+          </div>
 
-          {/* kecil kiri bawah */}
-          <Image
-            src={test}
-            alt="image 2"
-            className="w-full h-full object-cover rounded-xl"
-          />
+          <div className="relative rounded-xl overflow-hidden">
+            <Image src={test} alt="image 2" fill className="object-cover" />
+          </div>
         </div>
+
 
       </div>
     </section>
