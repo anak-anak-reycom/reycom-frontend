3be8755ad5cbfe92c25b6328963021b809ab3e7f
RodyZack07 <wildanad466@gmail.com>
2026-01-12 06:33:38 +0700

    add footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+    <footer className="w-full bg-[#234b68] text-white">
+        <div className="max-w-[1400px] mx-auto px-6 py-16">
+
+            //=== TOP SECTION ===
+            <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-4 gap-12">
+                
+
+            </div>
+            
+        </div>
+
+    </footer>
+
+    );
+
+
+}
