503aabf68777b17aba84be437d3f1959d32779e2
RodyZack07 <wildanad466@gmail.com>
2026-01-12 04:12:12 +0700

    add img-news

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero/Hero";
 import { Main } from "./components/main/main";
 import { CardDetail } from "./components/card/card";
 import  Vision  from "./components/vision/vision";
+import { NewsDetail } from "./components/news/news";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
       <Main />
       <CardDetail />
       <Vision />
+      <NewsDetail />
+      
       
     </div>
   );
